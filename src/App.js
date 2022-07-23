1fd20bec26c5c8538886862e49ff344cb555d1da
Gyungmin Myung <ryan000502@gmail.com>
2022-07-23 23:20:30 +0900

    useEffect tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import {useState} from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-  console.log("render")
   const [counter, setCounter] = useState(0);
-  const onClick = ()=>{
-    setCounter((prev)=>(prev+1))
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setCounter((prev) => (prev + 1));
   };
-  console.log("call an api")
+  useEffect(() => {
+    console.log("i run only once")
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 3) {
+      console.log("i run when the keyword changes");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when counter changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when keyword and counter changes");
+  }, [counter, keyword]);
   return (
     <div className="App">
+      <input
+        type="text"
+        placeholder="Search here..."
+        onChange={
+          (e) => setKeyword(e.target.value)
+        }
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
