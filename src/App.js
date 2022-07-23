8c12089256ba3dcef1cb697688c2d4f64013e7ba
Gyungmin Myung <ryan000502@gmail.com>
2022-07-23 22:50:45 +0900

    react_tutorial_2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
 
 function App() {
+  console.log("render")
+  const [counter, setCounter] = useState(0);
+  const onClick = ()=>{
+    setCounter((prev)=>(prev+1))
+  };
+  console.log("call an api")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
