3c4ed39ffd19ee0e07cbdfa1a24c258d3b7ef457
Gyungmin Myung <ryan000502@gmail.com>
2022-07-24 12:46:04 +0900

    coinConverter finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react"
 function App() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [amount, setAmount] = useState(0);
+  const [coin, setCoin] = useState("");
+  const [inverted, setInverted] = useState(false);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then(res => res.json())
@@ -11,13 +14,57 @@ function App() {
         setLoading(false);
       });
   }, []);
+  const onSelect = event => {
+    setCoin(event.target.value);
+    console.log(coin);
+  }
   return (
     <div>
+
       <h1>The Coins! ({coins.length})</h1>
-      {loading ? <p>Loading...</p> : null}
-      <ul>
-        {coins.map((coin, index)=><li key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price}</li>)}
-      </ul>
+      {loading ? <p>Loading...</p> :
+        <select name="coinSelector" id="coinSelector"
+          onChange={onSelect}>
+          <option value="" key="">Select a coin</option>
+          {coins.map((coin, index) => <option key={index} value={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price}</option>)}
+        </select>
+      }
+      {coin === "" ? null : <div>
+        <label htmlFor="coinamount">{coins[coin].name}</label>
+        <input
+          type="number"
+          id="coinamount"
+          onChange={event => {
+            if (!inverted) {
+              setAmount(event.target.value)
+            }
+          }}
+          disabled={inverted}
+          value={!inverted? amount: amount / coins[coin].quotes.USD.price}
+        />
+        <div>
+          <label htmlFor="fiatamount">USD</label>
+          <input
+            type="number"
+            id="fiatamount"
+            onChange={event => {
+              if (inverted) {
+                setAmount(event.target.value)
+              }
+            }}
+            value={inverted? amount : coins[coin].quotes.USD.price * amount}
+            disabled={!inverted}
+          />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setInverted((prev)=>!prev);
+              setAmount(0);
+            }}
+          >Flip</button>
+        </div>
+      </div>}
     </div>
   );
 }
